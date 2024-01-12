84c0e0baf1f51044479f0922f7766dee7016233b
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-01-12 09:50:57 +0530

    added successfully

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./components/homecomponent";
+import Quiz from "./components/quizcomponent";
+import Result from "./components/resultcomponent";
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <Quiz/>
+    <Result/>
+    </>
+  )
+}
+
+export default App
